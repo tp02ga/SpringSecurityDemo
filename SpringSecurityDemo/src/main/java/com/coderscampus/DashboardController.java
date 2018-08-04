3b2718dbf64967c65e3772e9eadda6e4625aa244
Trevor Page <trevor@craftycodr.com>
2018-08-04 15:16:01 -0400

    Creating Spring Boot App with User domain

---
+++ SpringSecurityDemo/src/main/java/com/coderscampus/DashboardController.java
@@ -0,0 +1,12 @@
+package com.coderscampus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+  @GetMapping("/dashboard")
+  public String getDashboard () {
+    return "dashboard";
+  }
+}
---
+++ SpringSecurityDemo/src/main/java/com/coderscampus/SpringSecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+}
