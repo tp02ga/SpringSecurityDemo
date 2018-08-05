89c2a5baa43832e41c580e5bf761e409b6e7ff7f
Trevor Page <trevor@craftycodr.com>
2018-08-05 14:37:26 -0400

    Exploring the AuthenticationPrincipal annotation

--- SpringSecurityDemo/src/main/java/com/coderscampus/web/DashboardController.java
+++ SpringSecurityDemo/src/main/java/com/coderscampus/web/DashboardController.java
@@ -1,12 +1,21 @@
 package com.coderscampus.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.coderscampus.domain.User;
 
 @Controller
 public class DashboardController {
+  
   @GetMapping("/dashboard")
-  public String getDashboard () {
+  public String getDashboard (@AuthenticationPrincipal User user, ModelMap model) {
+    System.out.println(user);
+    
+    model.put("user", user);
+    
     return "dashboard";
   }
 }
